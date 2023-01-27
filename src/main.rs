35f985e2de8bd482ff5f265ff5479a494717f53a
edd255 <abaddon@disroot.org>
2023-01-27 17:29:55 +0100

    refactor: fixed various pedantic warnings

--- src/main.rs
+++ src/main.rs
@@ -10,56 +10,56 @@ use rand::Rng;
 #[derive(Parser)]
 #[command(author, version, about, long_about)]
 struct Cli {
-	#[arg(short, long)]
-	filename: String,
+    #[arg(short, long)]
+    filename: String,
 
-	#[command(subcommand)]
-	command: Option<Commands>,
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-	SeamCarve {
-		#[arg(short, long)]
-		iterations: i32,
-	},
-	Statistics {},
+    SeamCarve {
+        #[arg(short, long)]
+        iterations: i32,
+    },
+    Statistics {},
 }
 
 fn main() {
-	let cli = Cli::parse();
-	match &cli.command {
-		Some(Commands::SeamCarve { iterations }) => {
-			println!("Filename:   {}", cli.filename);
-			println!("Iterations: {}", *iterations);
-		}
-		Some(Commands::Statistics {}) => {
-			let image = Image::read_file(cli.filename);
-			image.statistics();
-		}
-		None => {}
-	}
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::SeamCarve { iterations }) => {
+            println!("Filename:   {}", cli.filename);
+            println!("Iterations: {}", *iterations);
+        }
+        Some(Commands::Statistics {}) => {
+            let image = Image::read_file(cli.filename);
+            image.statistics();
+        }
+        None => {}
+    }
 
-	let width: usize = 1000;
-	let height: usize = 1000;
-	let mut pixels: Vec<Pixel> = Vec::with_capacity(width * height);
+    let width: usize = 1000;
+    let height: usize = 1000;
+    let mut pixels: Vec<Pixel> = Vec::with_capacity(width * height);
 
-	for _ in 0..height {
-		for _ in 0..width {
-			let red: u8 = rand::thread_rng().gen();
-			let green: u8 = rand::thread_rng().gen();
-			let blue: u8 = rand::thread_rng().gen();
-			let pixel: Pixel = Pixel { red, green, blue };
-			pixels.push(pixel);
-		}
-	}
+    for _ in 0..height {
+        for _ in 0..width {
+            let red: u8 = rand::thread_rng().gen();
+            let green: u8 = rand::thread_rng().gen();
+            let blue: u8 = rand::thread_rng().gen();
+            let pixel: Pixel = Pixel { red, green, blue };
+            pixels.push(pixel);
+        }
+    }
 
-	let image: Image = Image {
-		magic_number: "P3".to_string(),
-		width,
-		height,
-		scale: 255,
-		pixels,
-	};
-	image.write_file("test.ppm".to_string());
+    let image: Image = Image {
+        magic_number: "P3".to_string(),
+        width,
+        height,
+        scale: 255,
+        pixels,
+    };
+    image.write_file("test.ppm".to_string());
 }
